bd37f4fd2b7d0b0007593da07f9790883abce5b7
gycsaba <gyuris.csaba@vasvari.org>
2021-05-05 10:53:53 +0200

    Projekt létrejött

---
+++ 03-03-01-grid-and-simple-binding/App.xaml.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace _03_03_01_grid_and_simple_binding
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+    }
+}
