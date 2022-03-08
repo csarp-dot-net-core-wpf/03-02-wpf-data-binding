9e9ac7abff9d91438bc2b347089bf994424bd881
Csaba Gyuris <gyuris.csaba@vasvari.org>
2022-03-08 14:53:30 +0100

    MVVM.04 MVVM

---
+++ 03-03-02-mvvm-stackpanel-binding/ProjectWPFSquare/ViewModel/SquareViewModel.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProjectWPFSquare.ViewModel
+{
+    /// <summary>
+    /// MVVM.04
+    /// Az MVVM olyan tervezési minta, amelynek a célja, hogy teljes egészében elválassza a megjelenítést és a mögötte lévő tevékenységeket.
+    /// ViewModel
+    /// A nézetmodell lehetőséget ad a modell változásainak követésére és tevékenységek végrehajtására
+    /// SquareViewModel osztály
+    /// Az osztályban megatalálhatók azok a tulajdonságok amelyeket meg akarunk jeleníteni az ablakban
+    /// </summary>
+    class SquareViewModel
+    {
+    }
+}
