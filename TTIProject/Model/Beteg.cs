b103da95bcd2750a614361a7270d6fc54dc70846
Csaba Gyuris <gyuris.csaba@vasvari.org>
2023-09-12 09:22:38 +0200

    Beteg osztály adattagjai

--- TTIProject/Model/Beteg.cs
+++ TTIProject/Model/Beteg.cs
@@ -2,5 +2,17 @@
 {
     public class Beteg
     {
+        /// <summary>
+        /// Beteg neve
+        /// </summary>
+        private string nev;
+        /// <summary>
+        /// Beteg súlya (kg)
+        /// </summary>
+        private double suly;
+        /// <summary>
+        /// Beteg magassága (méter)
+        /// </summary>
+        private double magassag;
     }
 }
